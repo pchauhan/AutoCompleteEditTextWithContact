808b4d0eb224d50b328969a67130dfa37b018a43
pchauhan <parag.chauhan2010@gmail.com>
2015-11-30 22:42:43 +0530

    Init

---
+++ app/src/main/java/com/sn/autocompleteedittextwithcontact/Contact_Object.java
@@ -0,0 +1,9 @@
+package com.sn.autocompleteedittextwithcontact;
+
+import android.net.Uri;
+
+public class Contact_Object {
+	public String name , mobileNumber ,phoneNumber,uri;
+	public int type ;
+	//public Uri uri ;
+}
